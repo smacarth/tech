37e2611b3d7a6344f80858bc71930a2035eacec8
lenovo <lenovo@inkba.com>
2013-09-26 14:35:46 -0700

    Fixed syntax, added input

--- game.lua
+++ game.lua
@@ -1,4 +1,4 @@
-  print("Welcome to Energy Quest! Use left(), right(), down(), and up() to move. Use print_grid() to show the grid, and get_energy() to get your current energy level.")
+  print("Welcome to Energy Quest! Use left, right, down, and up to move. Use print_grid to show the grid, and get_energy to get your current energy level. Quit to quit.")
 
   --get locations of player and food
   local function get_x_location()
@@ -18,7 +18,7 @@
   end
   
   local function get_energy()
-    return energy
+    print(energy)
   end
   --set locations of player
   function set_x_location(x)
@@ -29,13 +29,17 @@
     y_location = y
   end
   
+  function set_energy(e)
+  	energy = e
+  end
+  
   --moving along grid
   function up()
-    move(0,1)
+    move(0,-1)
   end
   
   function down()
-    move(0,-1)
+    move(0,1)
   end
   
   function left()
@@ -46,13 +50,13 @@
     move(1,0)
   end
   
-  function move(x_incr, y_inrc)
-    energy = energy -1
-    new_x_location = get_x_location() + x_incr
-    new_y_location = get_y_location() + y_incr
+  function move(x_incr, y_incr)
+    set_energy(energy -1)
+    local new_x_location = get_x_location() + x_incr
+    local new_y_location = get_y_location() + y_incr
   
     --check if off grid/invalid move
-    if new_x_location() > 5 or new_x_location() < 0 or new_y_location() > 5 or new_y_location < 0 then
+    if new_x_location > 5 or new_x_location < 0 or new_y_location > 5 or new_y_location < 0 then
       invalid_move()
       return
     else -- valid location, move player
@@ -91,15 +95,18 @@
   
   -- show the grid to the user
   function print_grid()
-    for y_pos = 0, 5, 1 do
-      for x_pos = 0, 5, 1 do
+    for y_pos = 0, 4, 1 do
+    row = ""
+      for x_pos = 0, 4, 1 do
         --player is in this location
          if get_x_location() == x_pos and get_y_location() == y_pos then
-           print("[X]")
+          row = row.."[X]"
          else --player is not in location
-           print("[ ]")
+          row = row.."[ ]"
          end
+         
       end
+      print(row)
       print "\n" -- end of row
     end
   end
@@ -121,12 +128,41 @@
     os.exit()
   end 
   
+  function get_input()
+	local answer
+	repeat
+   	io.write("Your move?  ")
+   	io.flush()
+   	answer=io.read()
+   	if answer == "left" then
+   		left()
+   	
+    elseif answer == "right" then
+    	right()
+    	
+    elseif answer == "up" then
+    	up()
+    	
+    elseif answer == "down" then
+    	down()
+    	
+    elseif answer == "print_grid" then
+     	print_grid()
+     
+    elseif answer == "get_energy" then
+     	get_energy()
+	end
+	until answer == "quit"
+		quit()
+  end
+
    --player start in random location on 5x5 grid
-  local x_location = math.random(5)
-  local y_location = math.random(5)
+  x_location = math.random(5)
+  y_location = math.random(5)
   
   --food starts in random location on grid
   place_food()
   
   --starting energy
-  local energy = 50
+  energy = 50
+  get_input()
